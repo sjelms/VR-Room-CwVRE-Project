3c56af76555fa7dd40caf2b7454de53ab8d003e8
Stephen Elms <stephen@sjelms.com>
2022-08-01 20:45:49 +0100

    Add Clock and VR Sim Controls
    Clock element added but not working with script.

---
+++ Assets/Scipts (Custom)/Clock.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+   
+   public GameObject Clock_Analog_A_Hour; 
+   public GameObject Clock_Analog_A_Minute; 
+   public GameObject Clock_Analog_A_Second; 
+   
+   
+    void Update()
+    {
+        
+    }
+}
